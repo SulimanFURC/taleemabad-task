c0c8dd76c1d3324a62f5cb3fb332af32ea01f154
SulimanFURC <sullaimaan@gmail.com>
2022-05-04 19:57:48 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikiService } from './wiki.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'taleemabad-task';
+  title = 'Taleemabad - Task';
+  searchTerm: any;
+  results: any = [];
+
+  constructor(private wiki:WikiService) {}
+
+  onSubmit(){
+    this.wiki.search(this.searchTerm).subscribe( (res:any) =>{
+     this.results = res.query.search;
+     console.log(this.results);
+    })
+  }
+
 }
